2782834ebe397a91caa819547703374a504190b8
sultan <malsagov.sa@inbox.ru>
2021-06-24 11:59:01 +0300

    2zadanie

---
+++ print-arrays.js
@@ -0,0 +1,17 @@
+
+let a = []
+console.log(0)
+let b = [3, 4, 5]
+console.log(b[0] +b[2])
+let c = ["deada", "efief", "fesfwf"]
+console.log(c[0] + c[1] + c[2])
+let d = ["efefs", "fsfsfs", "efseffs", 3, 2, 1]
+console.log(`первый элемент массива ${d[0]}, а последний элемент массива ${d[5]}`)
+let e = ['Rakhim','Abdulla', 'Arsamak', 'Sally', 'Mansur']
+console.log(e[4], b[2] )
+let f = [3, 'faaefa', null, undefined, true, false]
+console.log(f[0], f[1], f[2], f[3], f[4], f[5] )
+let g = [[], [], []]
+console.log(g.length)
+let h = [[1, 2, 3], [4, 5, 6]]
+console.log(h[0][1] * h[1][2])
